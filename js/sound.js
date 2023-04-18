02f1a3a2be02bb36669abf4960f48fc92abd353d
Serhii Myshko <sergeiown@gmail.com>
2023-04-18 15:26:48 +0300

    add locale sounds

--- js/sound.js
+++ js/sound.js
@@ -1,8 +1,8 @@
 'use strict';
 
 export default function addRainSound() {
-    const rainElement = new Audio('https://zvukipro.com/index.php?do=download&id=11782');
-    const thunderElement = new Audio('https://zvukipro.com/index.php?do=download&id=792');
+    const rainElement = new Audio('../assets/sound/rain.mp3');
+    const thunderElement = new Audio('../assets/sound/thunder.mp3');
     let thunderTimeoutId;
 
     const playRain = (audio) => {
@@ -23,30 +23,32 @@ export default function addRainSound() {
         console.log(`Next thunder at ${Math.round(delay / 1000, 0)} seconds`);
     };
 
-    const button = document.createElement('button');
-    button.textContent = '🔇';
-    button.style.fontSize = '3rem';
-    button.title = 'Turn on sound';
-    button.style.backgroundColor = 'transparent';
-    button.style.opacity = '0.75';
-    button.style.position = 'fixed';
-    button.style.top = '10px';
-    button.style.left = '10px';
+    if (rainElement && thunderElement) {
+        const button = document.createElement('button');
+        button.textContent = '🔇';
+        button.style.fontSize = '3rem';
+        button.title = 'Turn on sound';
+        button.style.backgroundColor = 'transparent';
+        button.style.opacity = '0.75';
+        button.style.position = 'fixed';
+        button.style.top = '10px';
+        button.style.left = '10px';
 
-    button.addEventListener('click', () => {
-        if (rainElement.paused) {
-            playRain(rainElement);
-            delayThunder(thunderElement);
-            button.textContent = '🔊';
-            button.title = 'Turn off sound';
-        } else {
-            rainElement.pause();
-            thunderElement.pause();
-            button.textContent = '🔇';
-            button.title = 'Turn on sound';
-            clearTimeout(thunderTimeoutId);
-        }
-    });
+        button.addEventListener('click', () => {
+            if (rainElement.paused) {
+                playRain(rainElement);
+                delayThunder(thunderElement);
+                button.textContent = '🔊';
+                button.title = 'Turn off sound';
+            } else {
+                rainElement.pause();
+                thunderElement.pause();
+                button.textContent = '🔇';
+                button.title = 'Turn on sound';
+                clearTimeout(thunderTimeoutId);
+            }
+        });
 
-    document.body.appendChild(button);
+        document.body.appendChild(button);
+    }
 }
