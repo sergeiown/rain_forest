4e8dcd6d3ff85a43fcccd6ac3d7380a7c0cfa5b1
Serhii Myshko <sergeiown@gmail.com>
2024-04-07 15:36:16 +0300

    update visualization of drops

--- js/visual.js
+++ js/visual.js
@@ -6,7 +6,7 @@ export default function addRain() {
 
     for (let index = 0; index < 100; index++) {
         const randomAnimate = Math.floor(Math.random() * 98 + 2);
-        const randomBottom = Math.floor(Math.random() * 4 + 2);
+        const randomBottom = Math.floor(Math.random() * 38 + 2);
 
         drops += `
             <div class="drop" style="
